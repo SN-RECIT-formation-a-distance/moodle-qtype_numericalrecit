388759d4e378f2956abc9ac3e6ef2abbf96fd82e
Gustavo Bazzo <gcbazzo@gmail.com>
2021-05-04 10:35:21 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -25,14 +25,14 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_numericalrecit';
-$plugin->version   = 2021031600;
+$plugin->version   = 2021050400;
 $plugin->cron      = 0;
 $plugin->requires  = 2018050800;
 $plugin->dependencies = array(
     'qbehaviour_adaptive' => 2015111600,
     'qbehaviour_adaptivemultipart'     => 2014092500,
     'qtype_multichoice' => 2015111600,
-	'atto_recittakepicture' => 2021030200
+	'atto_recittakepicture' => 2021050400
 );
 $plugin->release   = 'R13-1.13.0';
 $plugin->maturity  = MATURITY_ALPHA;
