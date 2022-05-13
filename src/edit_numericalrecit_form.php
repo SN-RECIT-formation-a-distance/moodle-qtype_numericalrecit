8c9e89322456043869c17abadac8612c1f3b36c6
Émile Jacques <emilevirus@gmail.com>
2022-05-13 14:05:01 -0400

    Fix missing strings

--- src/edit_numericalrecit_form.php
+++ src/edit_numericalrecit_form.php
@@ -55,6 +55,8 @@ class qtype_numericalrecit_edit_form extends question_edit_form {
         $PAGE->requires->string_for_js('unit', 'qtype_numericalrecit');
         $PAGE->requires->string_for_js('valuetable', 'qtype_numericalrecit');
         $PAGE->requires->string_for_js('docs', 'qtype_numericalrecit');
+        $PAGE->requires->string_for_js('calculate', 'qtype_numericalrecit');
+        $PAGE->requires->string_for_js('descriptionhere', 'qtype_numericalrecit');
         $PAGE->requires->js('/question/type/numericalrecit/script/formatcheck.js');
         $PAGE->requires->css('/question/type/numericalrecit/styles.css');
         // Hide the unused form fields.
